e96f94d772f8a19ef6bdecf93500ef94293ea204
vlasov <stea1th@mail.ru>
2020-01-09 18:00:13 +0100

    fix

--- src/test/java/de/stea1th/greetings/repository/PersonRepositoryImplTest.java
+++ src/test/java/de/stea1th/greetings/repository/PersonRepositoryImplTest.java
@@ -7,58 +7,56 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest
+@ActiveProfiles("test")
 class PersonRepositoryImplTest {
-//
-//    @Autowired
-//    private TestEntityManager entityManager;
-//
-//    @Autowired
-//    private PersonRepository personRepository;
-//
-//    private Person person;
-//
-//    @BeforeEach
-//    void initTest() {
-//        person = new Person();
-//        person.setId(1000);
-//        person.setFirstName("Vadim");
-//        person.setLastName("Pupkin");
-//        person.setDateOfBirth(LocalDate.now());
-//        person.setEmail("hallo@web.de");
-//        person.setAutoEmailGreetings(true);
-//    }
-//
-//    @AfterEach
-//    void cleanTest() {
-//        entityManager.clear();
-//    }
-//
-//
-//    @Test
-//    void get() {
-//        entityManager.persist(person);
-//        entityManager.flush();
-//
-//        Person current = personRepository.get(1000);
-//
-//        assertEquals(person, current);
-//    }
-//
-//    @Test
-//    void getAllByDateOfBirth() {
-//    }
-//
-//    @Test
-//    void getAll() {
-//    }
-//
-//    @Test
-//    void save() {
-//    }
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    private Person person;
+
+    @BeforeEach
+    void initTest() {
+        person = new Person();
+        person.setFirstName("Chupa");
+        person.setLastName("Pupkin");
+        person.setDateOfBirth(LocalDate.now());
+        person.setEmail("hallo@web.de");
+        person.setAutoEmailGreetings(true);
+
+    }
+
+    @AfterEach
+    void cleanTest() {
+    }
+
+
+    @Test
+    void get() {
+        personRepository.save(person);
+
+        Person current = personRepository.get(1005);
+
+        assertEquals(person, current);
+    }
+
+    @Test
+    void getAllByDateOfBirth() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
 }
