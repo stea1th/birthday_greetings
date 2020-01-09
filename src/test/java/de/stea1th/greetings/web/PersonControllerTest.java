2d48eb94ac285331c7df1efb3772f4db19028d05
vlasov <stea1th@mail.ru>
2020-01-09 10:01:27 +0100

    pom spring test

---
+++ src/test/java/de/stea1th/greetings/web/PersonControllerTest.java
@@ -0,0 +1,23 @@
+package de.stea1th.greetings.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PersonControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllByDateOfBirth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toggleEmail() {
+    }
+}
